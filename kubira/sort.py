de0b6d822c5dd8782393dadcdd97b13f20c5b8d5
Radim Kubis <rkubis@redhat.com>
2018-11-19 10:07:31 +0100

    FIX(sort): Avoid duplicates of same name (same second)

--- kubira/sort.py
+++ kubira/sort.py
@@ -11,6 +11,12 @@ for img in os.listdir('.'):
     try:
         print tags['EXIF DateTimeOriginal']
         datas = str(tags['EXIF DateTimeOriginal']).replace(" ", ":").split(":")
-        os.rename('./'+img, datas[0]+datas[1]+datas[2]+"_"+datas[3]+datas[4]+datas[5]+".jpg")
+        new_name = datas[0]+datas[1]+datas[2]+"_"+datas[3]+datas[4]+datas[5]+".jpg"
+        for i in ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]:
+            if os.path.exists(new_name):
+                new_name = datas[0]+datas[1]+datas[2]+"_"+datas[3]+datas[4]+datas[5]+i+".jpg"
+            else:
+                break
+        os.rename('./'+img, new_name)
     except:
         continue
